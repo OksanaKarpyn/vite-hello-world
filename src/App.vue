9e3a615ac11e5eeaacede522e4f393624fb53a6a
Oksana Karpyn <oksana101992@gmail.com>
2023-05-02 16:12:08 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import AppTitle from './components/AppTitle.vue'
+export default {
+    name:'AppVue',
+    components:{
+
+        AppTitle
+    }
+}
+
+</script>
+
+<template>
+<div>
+    <h1>ciao mondo</h1>
+    <AppTitle/>
+    <h2>come stai</h2>
+</div>
+
+</template>
+
+
+<style scoped>
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+<script>
+
+export default{
+    name: "AppTitle",
+    data (){
+        return{
+            
+        }
+    }
+}
+</script>
+
+<template>
+
+<div>
+  <h2>La mia prima app con Vite</h2>
+</div>
+
+</template>
+
+<style>
+</style>
